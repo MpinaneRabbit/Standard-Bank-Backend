1bf99511764aa7653e6ca6c92bd9fdb13282e53b
Mpinane <mmohale@retrorabbit.co.za>
2019-02-06 13:50:10 +0200

    template project uploaded

--- FunctionApp1/FunctionApp1/Function1.cs
+++ FunctionApp1/FunctionApp1/Function1.cs
@@ -6,7 +6,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 
-namespace MpinaneBackend
+namespace FunctionApp1
 {
     public static class Function1
     {
